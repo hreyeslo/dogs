6366b9b58e738db359ba2a94f3755f7a0bf3e4c7
Héctor Reyes López <hreyeslo@everis.com>
2020-02-14 14:00:52 +0100

    Add: e2e test

--- e2e/src/integration/app.spec.ts
+++ e2e/src/integration/app.spec.ts
@@ -1,9 +1,37 @@
-import { getGreeting } from '../support/app.po';
+import { getTitle, changeLang, selectBreed, imageList } from '../support/app.po';
 
 describe('dogs', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+    changeLang('es');
+    cy.server();
+  });
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Buscador de perros');
+  afterEach(() => {
+    changeLang('es');
+  });
+
+  describe('should change language', () => {
+    it('spanish', () => {
+      getTitle().contains('Buscador de perros');
+    });
+
+    it('english', () => {
+      changeLang('en');
+      getTitle().contains('Dogs finder');
+    });
+  });
+
+  describe('should fetch images', () => {
+    it('random images', () => {
+      cy.route('GET', '**/images/random/**').as('images');
+      selectBreed();
+      cy.wait('@images');
+      imageList()
+        .its('length')
+        .then(length => {
+          expect(length).not.to.equal(0);
+        });
+    });
   });
 });
